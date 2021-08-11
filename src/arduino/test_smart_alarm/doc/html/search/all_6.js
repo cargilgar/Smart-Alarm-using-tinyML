16592e09ca1610cbc64da3e09e8c8d802c31382e
cargilgar <cargilgar6@gmail.com>
2021-08-11 13:26:16 +0200

    removed doc

--- src/arduino/test_smart_alarm/doc/html/search/all_6.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['output_5fhandler_2ecpp_0',['output_handler.cpp',['../output__handler_8cpp.html',1,'']]],
-  ['output_5fhandler_2eh_1',['output_handler.h',['../output__handler_8h.html',1,'']]]
-];
--- src/arduino/test_smart_alarm/doc/html/search/files_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['test_5fsmart_5falarm_2eino_0',['test_smart_alarm.ino',['../test__smart__alarm_8ino.html',1,'']]]
-];
