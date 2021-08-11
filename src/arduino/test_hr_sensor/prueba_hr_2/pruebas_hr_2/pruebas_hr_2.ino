2864f719eac2ad2ef494f52fc75402648636c730
cargilgar <cargilgar6@gmail.com>
2021-08-12 00:04:34 +0200

    worked on arduino

--- src/arduino/test_hr_sensor/prueba_hr_2/pruebas_hr_2/pruebas_hr_2.ino
+++ src/arduino/test_hr_sensor/prueba_hr_2/pruebas_hr_2/pruebas_hr_2.ino
@@ -1,69 +1,70 @@
-const int sensorPin = A0;                          // A0 is the input pin for the heart rate sensor
-
-float sensorValue = 0;                             // Variable to store the value coming from the sensor
-float hr = 0;
-
-unsigned long starttime = 0;
-unsigned long IBI = 0;                            // 80 BPM
-unsigned long last_time = 0;
-
-int heartrate = 0;
-int BPM = 0;
-int count = 0;
-int threshold = 550; 
-
-boolean counted = false;                          //Adjust threshold fo a correct measurement
-
-
-void setup () {
-
-Serial.begin (9600);                              // Start Serial Communication @ 9600
-
-}
-
-void loop (){
-
-
-starttime = millis();
-
-while (millis() < (starttime + 15000)){                  // Reading pulse sensor for 5 seconds
-
-sensorValue = analogRead(sensorPin);
-IBI = millis();                                         // Storage IBI to calculate BPM
- 
-    if ((sensorValue >= threshold) && (counted == false) && ((IBI - last_time) > 420)){  // Threshold value is 530 (~ 2.7V) 
+namespace{
+    const int sensorPin = A0;                          // A0 is the input pin for the heart rate sensor
     
-        last_time = IBI;                                // Storage last time to calculate BPM                          
-        
-        count++;                        
-        counted = true;
-        Serial.println (sensorValue);
-    }
-  
-    else if (sensorValue < threshold){
-        counted = false;  
-    }
-
-    if (count > 0){
-        hr = 60000 / (IBI - last_time);
-        //Serial.println (hr);
-        heartrate = heartrate + hr;
-        hr = 0;
-    }
-  
+    int sensorValue = 0;                             // Variable to store the value coming from the sensor
+    float hr = 0;
+    
+    unsigned long starttime = 0;
+    unsigned long IBI = 0;                            // 80 BPM
+    unsigned long last_time = 0;
+    
+    int heartrate = 0;
+    int BPM = 0;
+    int count = 0;
+    int threshold = 518; 
+    
+    bool counted = false;                          //Adjust threshold fo a correct measurement
 }
 
-Serial.print ("count = ");
-Serial.println (count);
+void setup(){
 
-//BPM = heartrate / count;
-BPM = count * 4;
+    Serial.begin(9600);                              // Start Serial Communication @ 9600
 
-Serial.print ("BPM = ");
-Serial.println (BPM);  
+    delay(100);
 
-count = 0;
-heartrate = 0;
-counted = false;
+}
+
+void loop(){
+
+    starttime = millis();
+
+    
+    while (millis() < (starttime + 15000)){                  // Reading pulse sensor for 5 seconds
+    
+        sensorValue = analogRead(sensorPin);
+        IBI = millis();                                         // Storage IBI to calculate BPM
+    /*
+        if (count > 0){
+            hr = 60000 / (IBI - last_time);
+            //Serial.println (hr);
+            heartrate = heartrate + hr;
+            hr = 0;
+        }*/
+         
+        if ((sensorValue >= threshold) && (!counted) && ((IBI - last_time) > 420)){  // Threshold value is 530 (~ 2.7V) 
+            
+            last_time = IBI;                                // Storage last time to calculate BPM                          
+            
+            count++;                        
+            counted = true;
+            Serial.println(sensorValue);
+        }  
+        else if (sensorValue < threshold){
+            counted = false;  
+        }     
+    }
+    
+    Serial.print("count = ");
+    Serial.println(count);
+    
+    //BPM = heartrate / count;
+    BPM = count * 4;
+    
+    Serial.print("BPM = ");
+    Serial.println(BPM);  
+    
+    count = 0;
+    heartrate = 0;
+    counted = false;
 
 }
