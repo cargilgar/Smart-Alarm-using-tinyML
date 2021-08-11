e3efdd1fb5529465c0bc25d5462dbc64b225b6fe
Daniel <danimp94@users.noreply.github.com>
2021-08-11 21:49:29 +0200

    testin_hr

---
+++ src/arduino/test_hr_sensor/adjust_threshold/adjust_threshold.ino
@@ -0,0 +1,29 @@
+//  Variables
+int PulseSensorPurplePin = 0;        // Pulse Sensor connected to ANALOG PIN 0
+int LED13 = 13;                      //  The on-board Arduion LED
+
+
+int Signal;                          // holds the incoming raw data. Signal value can range from 0-1024
+int Threshold = 550;                 // Determine which Signal to "count as a beat", and which to ingore.
+
+void setup() {
+  pinMode(LED13,OUTPUT);             // pin that will blink to your heartbeat
+   Serial.begin(9600);               // Set's up Serial Communication at certain speed.
+
+}
+
+void loop() {
+
+  Signal = analogRead(PulseSensorPurplePin);  // Read the PulseSensor's value.
+                                              // Assign this value to the "Signal" variable.
+   Serial.println(Signal);                    // Send the Signal value to Serial Plotter.
+   
+   if(Signal > Threshold){                          // If the signal is above "550", then "turn-on" Arduino's on-Board LED.
+     digitalWrite(LED13,HIGH);
+   } else {
+     digitalWrite(LED13,LOW);                //  Else, the sigal must be below "550", so "turn-off" this LED.
+   }
+
+delay(10);
+
+}
--- src/arduino/test_hr_sensor/prueba_hr_2/pruebas_hr_2/pruebas_hr_2.ino
+++ src/arduino/test_hr_sensor/prueba_hr_2/pruebas_hr_2/pruebas_hr_2.ino
@@ -1,22 +1,23 @@
-const int sensorPin = A0;                                // A0 is the input pin for the heart rate sensor
+const int sensorPin = A0;                          // A0 is the input pin for the heart rate sensor
 
 float sensorValue = 0;                             // Variable to store the value coming from the sensor
 float hr = 0;
 
 unsigned long starttime = 0;
-unsigned long IBI = 421;                           // 80 BPM
+unsigned long IBI = 0;                            // 80 BPM
 unsigned long last_time = 0;
 
 int heartrate = 0;
 int BPM = 0;
 int count = 0;
+int threshold = 550; 
 
-boolean counted = false;
+boolean counted = false;                          //Adjust threshold fo a correct measurement
 
 
 void setup () {
 
-Serial.begin (9600);                               // Start Serial Communication @ 9600
+Serial.begin (9600);                              // Start Serial Communication @ 9600
 
 }
 
@@ -24,47 +25,45 @@ void loop (){
 
 
 starttime = millis();
-Serial.println ("1");
 
-while (millis() < (starttime + 5000)){                  // Reading pulse sensor for 5 seconds
+while (millis() < (starttime + 15000)){                  // Reading pulse sensor for 5 seconds
 
 sensorValue = analogRead(sensorPin);
-//Serial.println (sensorValue);
-
-    if ((sensorValue >= 530) && (counted == false) && ((IBI - last_time) > 420))  // Threshold value is 530 (~ 2.7V) 
+IBI = millis();                                         // Storage IBI to calculate BPM
+ 
+    if ((sensorValue >= threshold) && (counted == false) && ((IBI - last_time) > 420)){  // Threshold value is 530 (~ 2.7V) 
     
-    {
-        last_time = IBI;                  // Storage last time to calculate BPM
-        IBI = millis();                   // Storage IBI to calculate BPM
+        last_time = IBI;                                // Storage last time to calculate BPM                          
         
-        count++;
-    
+        count++;                        
         counted = true;
+        Serial.println (sensorValue);
     }
   
-  
-    else if (sensorValue < 530){
-    
-        counted = false;
-        
+    else if (sensorValue < threshold){
+        counted = false;  
     }
 
     if (count > 0){
-    
         hr = 60000 / (IBI - last_time);
+        //Serial.println (hr);
         heartrate = heartrate + hr;
         hr = 0;
     }
   
 }
 
-BPM = heartrate / count;
+Serial.print ("count = ");
+Serial.println (count);
+
+//BPM = heartrate / count;
+BPM = count * 4;
 
 Serial.print ("BPM = ");
 Serial.println (BPM);  
 
 count = 0;
-counted = false;
 heartrate = 0;
+counted = false;
 
 }
