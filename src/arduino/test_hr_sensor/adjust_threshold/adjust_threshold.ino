3e3d572386ac82744912c10a75faff9e330f2888
carlos <cargilgar6@gmail.com>
2021-08-19 16:26:14 +0200

    worked on test hr

--- src/arduino/test_hr_sensor/adjust_threshold/adjust_threshold.ino
+++ src/arduino/test_hr_sensor/adjust_threshold/adjust_threshold.ino
@@ -6,23 +6,21 @@ int sensorValue;                      // Holds the incoming raw data. sensorValu
 int threshold = 346;                  // Determine which sensorValue to "count as a beat", and which to ingore.
 
 void setup() {
-  pinMode(blinkPin, OUTPUT);          // Pin that will blink 
-  Serial.begin(9600);                 // Set's up Serial Communication at certain speed. @9600
-
+    pinMode(blinkPin, OUTPUT);          // Pin that will blink
+    Serial.begin(9600);                 // Set's up Serial Communication at certain speed. @9600
 }
 
 void loop() {
 
-  sensorValue = analogRead(pulseSensorPin);    // Read the PulseSensor's value.
-                                               // Assign this value to the "sensorValue" variable.
-   Serial.println(sensorValue);                // Send the sensorValue to Serial Plotter.
-   
-   if(sensorValue > threshold){                // If the signal is above threshold value, then "turn-on" Arduino's on-Board LED.
-     digitalWrite(blinkPin,HIGH);
-   } else {
-     digitalWrite(blinkPin,LOW);               //  Else, the sensorValue must be below threshold value, so "turn-off" this LED.
-   }
+    sensorValue = analogRead(pulseSensorPin);   // Read the PulseSensor's value.
+                                                // Assign this value to the "sensorValue" variable.
+    Serial.println(sensorValue);                // Send the sensorValue to Serial Plotter.
 
-delay(10);
+    if(sensorValue > threshold)                 // If the signal is above threshold value, then "turn-on" Arduino's on-Board LED.
+        digitalWrite(blinkPin, HIGH);
+    else
+        digitalWrite(blinkPin, LOW);            //  Else, the sensorValue must be below threshold value, so "turn-off" this LED.
+
+    delay(10);
 
 }
--- src/arduino/test_hr_sensor/calculate_bpm/calculate_bpm.ino
+++ src/arduino/test_hr_sensor/calculate_bpm/calculate_bpm.ino
@@ -1,71 +1,63 @@
-    const int sensorPin = A0;                        // A0 is the input pin for the heart rate sensor
-    
-    int sensorValue = 0;                             // Variable to store the value coming from the sensor
-    int heartRate = 0;                               // Variable to calculate the mean of the detected beats
-    int BPM = 0;                                     // Variable to feed the interpreter (Beats per minute)
-    int count = 0;                                   // Variable to count detected beats
-    int threshold = 345;                             // Set threshold for correct measurement (check adjust_threshold.ino)
-    float hr = 0;                                    // Variable to calculate 1 BPM
-      
-    unsigned long startTime = 0;                     // Timer needed for while loop 
-    unsigned long IBI = 0;                           // Inter Beat Interval (ms)
-    unsigned long newBeatTime = 0;                   // Timer to Calculate IBI
-    unsigned long lastTime = 0;                      // Timer to Calculate IBI
-    unsigned long readingTime = 15000;               // Reading sensor Time (15 seconds)
-    
-    bool counted = false;                            // Boolean to avoid false positives when the wave decreases
+const int sensorPin = A0;                        // A0 is the input pin for the heart rate sensor
+
+int heartRate = 0;                               // Variable to calculate the mean of the detected beats
+int BPM = 0;                                     // Variable to feed the interpreter (Beats per minute)
+int count = 0;                                   // Variable to count detected beats
+int threshold = 345;                             // Set threshold for correct measurement (check adjust_threshold.ino)
+float hr = 0;                                    // Variable to calculate 1 BPM
+
+unsigned long startTime = 0;                     // Timer needed for while loop
+unsigned long IBI = 0;                           // Inter Beat Interval (ms)
+unsigned long newBeatTime = 0;                   // Timer to Calculate IBI
+unsigned long lastTime = 0;                      // Timer to Calculate IBI
+unsigned long readingTime = 15000;               // Reading sensor Time (15 seconds)
+
+bool counted = false;                            // Boolean to avoid false positives when the wave decreases
 
 void setup(){
 
     Serial.begin(9600);                              // Start Serial Communication @ 9600
-
 }
 
 void loop(){
 
     startTime = millis();
-    
-    while (millis() < (startTime + readingTime)){               // Reading pulse sensor for readingTime seconds
-    
-        sensorValue = analogRead(sensorPin);                 // Read SensorValue                                   
-        newBeatTime = millis();                              // Storage new time to calculate IBI 
-        
-        if ((sensorValue >= threshold) && (!counted) && ((newBeatTime - lastTime) > 420)){  // Check threshold, wave rising and IBI > 420 ms
 
-            // Serial.println(sensorValue);                  // Print Sensor value (just for checking) 
-            
-            IBI = newBeatTime - lastTime;                    // Calculate IBI
-            lastTime = millis();                             // Storage last time to calculate IBI                         
-            
-            count++;                                         // Add a heart beat detected to the counter
-            
-            counted = true;                                  // It is necessary to check that the wave decreases
-            
-            hr = (60000 / IBI);                              // Calculate 1 BPM
-            Serial.print("Calculated BPM = "); 
-            Serial.println(hr);                              // Print the calculated BPM value (just for checking)
-            heartRate = heartRate + hr;                      // Add BPMs calculated
-            hr = 0;                                          // Reset the value to use it again in the next beat
-           
-            
-        } else if (sensorValue < threshold){                 // Check the wave is decreasing below threshold value
-          
+    while (millis() < (startTime + readingTime)){            // Reading pulse sensor for readingTime seconds
+
+        newBeatTime = millis();                              // Storage new time to calculate IBI
+
+        if ((analogRead(sensorPin) >= threshold) {
+            if(!counted) && ((newBeatTime - lastTime) > 420)) {  // Check threshold, wave rising and IBI > 420 ms
+
+                IBI = newBeatTime - lastTime;                    // Calculate IBI
+                lastTime = millis();                             // Storage last time to calculate IBI
+
+                count++;                                         // Add a heart beat detected to the counter
+
+                hr = (60000 / IBI);                              // Calculate 1 BPM
+                heartRate += hr;                                 // Add BPMs calculated
+
+                Serial.print("Calculated BPM = ");
+                Serial.println(hr);                              // Print the calculated BPM value (just for checking)
+
+                counted = true;                                  // It is necessary to check that the wave decreases
+            }
+        }
+        else                // Check the wave is decreasing below threshold value
             counted = false;                                 // With the next beat it is necessary to check that the wave is rising
-        }       
-
     }
-    
+
     Serial.print("Beats detected = ");
     Serial.println(count);                                    // Print counted beats (just for checking)
-    
+
     BPM = heartRate / count;                                  // Mean of BPMs calculated (value needed to feed into the model)
     //BPM = count * (60000 / readingTime);                    // Simple method (less accurate)
-    
+
     Serial.print("BPM = ");
     Serial.println(BPM);                                      // Print BPM (just for checking)
-    
+
     count = 0;                                                // Reset counter
     heartRate = 0;                                            // Reset hearRate
     counted = false;                                          // Reset Boolean
-  
 }
